c7ccc838069706f88901e24e35753e33363fefe6
ZW Cai <zw.cai@mediatek.com>
2019-09-17 15:19:58 +0800

    20190917_1519

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+  $('.menu1').hide().fadeIn(100);
+
+	$("#info").mouseover(function(){
+        $("#info-menu").fadeIn(200);
+				$("#info").addClass("active");
+    });
+
+	$("#made").mouseover(function(){
+        $("#made-menu").fadeIn(200);
+				$("#made").addClass("active");
+    });
+
+	$("#store").mouseover(function(){
+        $("#made-menu").fadeOut(100);
+		$("#made").removeClass("active");
+    });
+
+	$(".link").click(function(){
+				$("#made").removeClass("active");
+        $("#made-menu").fadeOut(100);
+				$("#info").removeClass("active");
+			  $("#info-menu").fadeOut(100);
+    });
+
+	$("html").click(function(){
+				$("#made").removeClass("active");
+        $("#made-menu").fadeOut(100);
+				$("#info").removeClass("active");
+			  $("#info-menu").fadeOut(100);
+    });
+
+	$('.tooltip').tooltipster({
+	   	position: 'bottom-right',
+	   	arrow: false,
+	   	offsetX: '10px',
+	   	offsetY: '-20px',
+	   	touchDevices: false
+	});
+
+	function createMailtoLinks(){
+    $('A[data-u][href=""]').each(function(){
+        var i = $(this);
+        i.attr('href', 'mai'+'lto:'+i.data('u')+'@'+i.data('d'));
+        if (i.html()==''){ i.html(i.data('u')+'@'+i.data('d')); }
+    });
+	}
+	$(function(){
+		createMailtoLinks();
+	});
+
+});
